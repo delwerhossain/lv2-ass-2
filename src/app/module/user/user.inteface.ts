8f02123fa30267bc264f7ebf9c9c5d0594dd450a
Delwer Hossain <delwerhossain006@gmail.com>
2023-11-24 02:54:11 +0600

    interface create

---
+++ src/app/module/user/user.inteface.ts
@@ -0,0 +1,26 @@
+type FullName = {
+  firstName: string;
+  lastName: string;
+};
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+type Orders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+export interface User {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: FullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: Address;
+  orders: Orders[];
+}

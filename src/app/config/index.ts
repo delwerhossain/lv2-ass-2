cd64a509faba8cd2b30e106a64b1a7ba074a8d23
delwerhossain <delwerhossain006@gmail.com>
2023-11-24 10:39:04 +0000

    format

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,11 +1,9 @@
 import dotenv from 'dotenv';
-import path from "path"
+import path from 'path';
 
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.port,
   mongodb_url: process.env.mongodb_url,
 };
-

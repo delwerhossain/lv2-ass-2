afefb2da3019b694b8683bed7202b2398c3b95a3
delwerhossain <delwerhossain006@gmail.com>
2023-11-24 12:11:18 +0000

    error handle

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -2,13 +2,21 @@ import { Request, Response } from 'express';
 import { userServices } from './user.service';
 
 const createUser = async (req: Request, res: Response) => {
-  const user = req.body;
-  const result = await userServices.createUserIntoDB(user);
-  res.status(201).json({
-    success: true,
-    message: 'User created successfully',
-    data: result,
-  });
+  try {
+    const user = req.body;
+    const result = await userServices.createUserIntoDB(user);
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'error , something went wrong',
+      error,
+    });
+  }
 };
 
 export const userControllers = {

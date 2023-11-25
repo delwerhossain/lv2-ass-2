4e51583dfaccd3cbe5ceb8129841bd94d61474d5
delwerhossain <delwerhossain006@gmail.com>
2023-11-25 14:57:49 +0000

    start route create for check

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { userRoute } from './app/module/user/user.route';
 const app: Application = express();
@@ -7,5 +7,8 @@ app.use(express.text());
 app.use(cors());
 
 app.use('/api/users', userRoute);
+app.get('/', (req: Request, res: Response) => {
+    res.send('working Level 2 Assessment 2')
+})
 
 export default app;
